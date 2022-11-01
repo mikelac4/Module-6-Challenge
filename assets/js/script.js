70394586934f601a147956a577c4a6cff3000727
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2022-11-01 11:41:59 -0400

    JS edits 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,38 @@ var searchHistoryContainer = document.querySelector('#history');
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
+// Function to display the search history list.
+function renderSearchHistory() {
+    searchHistoryContainer.innerHTML = '';
+
+    //Start at end of history array and count down to show the most recent at the top.
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+
+        // `date-search` allows access to city name when click handler is invoked
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append[btn];
+    }
+}
+
+// Function to update history in local storage then updates displayed history.
+function appendToHistory(search) {
+    // If there is no search term return the function
+    if (searchHistory.indexOf(search) !== -1) {
+        return;
+    }
+}
+
 function renderItems(city, data) {
   // renderCurrentWeather(city, data.current, data.timezone);
   renderForecast(data.daily, data.timezone);
 }
 
-
+ 
 function fetchWeather(location) {
  	var { lat } = location;
   	var { lon } = location;
